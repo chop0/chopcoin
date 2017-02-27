a196f5a12af2130e3f56441edd72069e1b0b21bd
Alec Petridis <chop@quenda.net>
2017-02-27 15:51:32 -0800

    Fixed tiny syntax error

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"52.90.137.216", "52.90.137.216"} // My amazon aws instance,
+    {"52.90.137.216", "52.90.137.216"}, // My amazon aws instance,
     {NULL, NULL}
 };
 
