ce590b9d15b4ca7a5676ea7e5a53e613afcf9a8b
Ubuntu <ubuntu@ip-172-31-51-7.ec2.internal>
2018-02-07 23:33:40 +0000

    Changed seed node

--- src/net.cpp
+++ src/net.cpp
@@ -1173,7 +1173,7 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"52.90.137.216", "52.90.137.216"}, // My amazon aws instance,
+    {"54.174.247.123", "54.174.247.123"}, // My amazon aws instance,
     {NULL, NULL}
 };
 
